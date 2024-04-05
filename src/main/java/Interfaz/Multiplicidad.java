890c477cc0bd69b32ea0c194a4a4cf063bc47b11
mllangon <mllangon@myuax.com>
2024-04-05 11:21:07 +0200

    9

--- src/main/java/Interfaz/Multiplicidad.java
+++ src/main/java/Interfaz/Multiplicidad.java
@@ -1,4 +1,37 @@
 package Interfaz;
 
-public class Multiplicidad {
+import javax.swing.*;
+import java.awt.*;
+
+public class Multiplicidad extends JFrame {
+    private JTabbedPane pestañas;
+
+    public Multiplicidad() {
+        super("Multiplicidad de Documentos");
+        setSize(600, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        inicializarUI();
+    }
+
+    private void inicializarUI() {
+        pestañas = new JTabbedPane();
+
+        JTextArea areaTexto1 = new JTextArea();
+        pestañas.addTab("Documento 1", new JScrollPane(areaTexto1));
+
+        JTextArea areaTexto2 = new JTextArea();
+        pestañas.addTab("Documento 2", new JScrollPane(areaTexto2));
+
+        // Aquí puedes añadir más documentos o incluso permitir al usuario abrir nuevos documentos en nuevas pestañas.
+
+        add(pestañas);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            Multiplicidad frame = new Multiplicidad();
+            frame.setVisible(true);
+        });
+    }
 }
+
